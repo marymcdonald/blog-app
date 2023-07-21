ebd19c10e911906259d4dc5887fd90c631a5570a
marymcdonald <10123446+marymcdonald@users.noreply.github.com>
2023-07-21 15:36:22 -0400

    adding route handler module

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const config = require('./utils/config');
-const logger = require('./utils/logger');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const logger = require('./utils/logger');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+
+
 
 
 const mongoUrl = config.MONGODB_URI
@@ -13,24 +15,7 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('api/blogs', blogsRouter);
 
 
 app.listen(config.PORT, () => {
