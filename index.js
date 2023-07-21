6698dcaf9d3dd61e864951214930e5d885fdb219
marymcdonald <10123446+marymcdonald@users.noreply.github.com>
2023-07-21 15:16:25 -0400

    adding mongoose model

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-require('dotenv').config();
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-console.log('connecting to MongoDB');
+const mongoUrl = config.MONGODB_URI
+logger.info('connecting to MongoDB');
 mongoose.connect(mongoUrl)
 
 app.use(cors())
@@ -38,7 +32,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
