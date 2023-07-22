11ebcc8146ca5491c3d45aaa32aaedb45475429f
marymcdonald <10123446+marymcdonald@users.noreply.github.com>
2023-07-22 13:24:42 -0400

    adding a users route module

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('express-async-errors');
 const app = express();
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 
 const mongoose = require('mongoose');
@@ -24,6 +25,7 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 app.use(middleware.requestLogger);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
